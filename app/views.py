9db4983aa0704d225bb1e31f689c0ff1755552bb
Michael Ritsema <michael.ritsema@ziften.com>
2016-07-18 14:33:35 -0500

    IP drilldown

---
+++ app/views.py
@@ -0,0 +1,33 @@
+__author__ = "Carbon Black <support@carbonblack.com>"
+
+import json
+from app import app
+from qpylib import qpylib
+from flask import request, render_template, redirect, jsonify, Response
+import urllib
+import config
+from cbapi import CbApi
+from collections import defaultdict
+import urlparse
+import dateutil.parser
+from functools import wraps
+import requests
+import traceback
+import os
+import sqlite3
+import pprint
+
+
+base_url = 'https://sales.cloud.ziften.com'
+
+/ipaddresses/overview
+
+
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    ip = request.args.get('context')
+    query = 'ipaddr:%s' % ipaddr
+    url = base_url + 'ipaddresses/overview/' + ip 
+    return jsonify({'url': url})
