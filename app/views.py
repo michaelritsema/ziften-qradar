029db6d7d31f09242f8afecbd3ae9f7ec9482324
Michael Ritsema <michael.ritsema@ziften.com>
2016-07-19 08:52:49 -0500

    .

--- app/views.py
+++ app/views.py
@@ -2,13 +2,51 @@ __author__ = "Ziften <michael.ritsema@ziften.com>"
 
 from app import app
 from flask import request, render_template, redirect, jsonify, Response
+import settings
 
 
-base_url = 'https://sales.cloud.ziften.com'
+base_url = settings.get_base_url()
 
-@app.route('/ziftensearch', methods=['GET'])
-def zsearch():
-    ip = request.args.get('context')
-    url = base_url + '/ipaddresses/overview/' + ip 
-    #return ""
+md5_search_url = '/binaries/binaries?imagefilemd5__contains='
+filename_search_url = '/applications/applications?most_prevalent_filename__contains='
+system_search_url = '/systems/systems?system__contains='
+ip_search_url = '/ipaddresses/ipaddresses?ipaddress__cidr='
+hostname_search_url ='/hostnames/hostnames?hostname__contains='
+user_search_url = '/osusers/osusers?user_account__contains='
+
+
+@app.route('/usersearch', methods=['GET'])
+def userSearch():
+    c = request.args.get('context')
+    url = base_url + user_search_url + c 
+    return jsonify({'url': url})
+
+@app.route('/hostnamesearch', methods=['GET'])
+def hostnameSearch():
+    c = request.args.get('context')
+    url = base_url + hostname_search_url + c 
+    return jsonify({'url': url})
+
+@app.route('/systemsearch', methods=['GET'])
+def systemSearch():
+    c = request.args.get('context')
+    url = base_url + system_search_url + c 
+    return jsonify({'url': url})
+
+@app.route('/filenamesearch', methods=['GET'])
+def filenameSearch():
+    c = request.args.get('context')
+    url = base_url + filename_search_url + c 
+    return jsonify({'url': url})
+
+@app.route('/ipsearch', methods=['GET'])
+def ipSearch():
+    c = request.args.get('context')
+    url = base_url + ip_search_url + c 
+    return jsonify({'url': url})
+
+@app.route('/md5search', methods=['GET'])
+def md5Search():
+    c = request.args.get('context')
+    url = base_url + md5_search_url + c 
     return jsonify({'url': url})
